6d199876656c56831f3f647690055396d155ca6d
Mahdi Hasan Shuvo <98658558+Shuvo-BBHH@users.noreply.github.com>
2022-03-19 15:55:03 +0600

    Create shuvo.py

---
+++ shuvo.py
@@ -0,0 +1,76 @@
+import requests
+
+from requests.structures import CaseInsensitiveDict
+
+import os
+
+import sys
+
+import time
+
+os.system("pip install requests")
+
+os.system("clear")
+
+red="\033[0;31m"          # Red
+
+yellow="\033[0;33m"       # Yellow
+
+green="\033[0;32m"        # Green
+
+color_off="\033[0m"       # Text Reset
+
+bblack="\033[1;30m"       # Black
+
+bred="\033[1;31m"         # Red
+
+ured="\033[4;31m"         # Red
+
+on_green="\033[42m"       # Green
+
+blue="\033[0;34m"         # Blue
+
+lightblue = '\033[94m'
+
+red = '\033[91m'
+
+white = '\33[97m'
+
+yellow = '\33[93m'
+
+green = '\033[1;32m'
+
+cyan  = "\033[96m"
+
+end = '\033[0m'
+
+purple="\033[0;35m"
+
+def main():
+	os.system("clear")
+   print("""\033[0;45m.88b  d88.  .d8b.  db   db d8888b. d888888b\n88'YbdP`88 d8' `8b 88   88 88  `8D   `88'\n88  88  88 88ooo88 88ooo88 88   88    88\n88  88  88 88~~~88 88~~~88 88   88    88\n88  88  88 88   88 88   88 88  .8D   .88.\nYP  YP  YP YP   YP YP   YP Y8888D' Y888888P                                             
+\033[0m================================================================
+  \33[93mAUTHOR :\033[91m[MAHDI HASAN] SHUVO
+   \033[0;33mGITHUB : \033[1;97mhttps://github.com/Shuvo-BBHH
+   WELL COME SYLHET GAMG (IAM NEW MEMBER IN YOUR GRUP)
+  LIVE in Sylhet (Read in class 10)
+\033[42mNo NEED GF \033[0;31mIF YOU LOVE ME I LOVE YOU IF U HAT ME I FUCK YOU 
+================================================================""")
+     	
+print("""
+\033[42m[1] CLONE FROM 2009 ID
+\033[0;88m[2]CLONE FROM 2006-2009 ID 
+\033[0;88m[3]CLONE FROM 2010-2016 ID
+\033[0;33m[4]CLONE FROM  2010-2021 ID
+\033[0;32m[5] CLONE FROM 2010-2021 ID
+FRIENDLIST (ND TOKEN)
+\033[42m[6] CLONE FROM 2009 ID
+
+
+
+
+
+""")
+
+pil = input("\033[1;97m[\033[1;94m?\033[1;97m] CHOOSE: ")
+    
